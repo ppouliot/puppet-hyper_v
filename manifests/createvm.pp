c79ad21e0050ff3f43838a66beb8c55972bd5aff
Peter Pouliot <ppouliot@gmail.com>
2013-10-14 15:39:53 -0400

    old create vm class

---
+++ manifests/createvm.pp
@@ -0,0 +1,86 @@
+class createvm {
+
+
+
+
+
+        $powershell_path        = "c:\\Windows\\sysnative\\WindowsPowerShell\\v1.0\\"
+        $path_extend            = "c:\\Windows;c:\\Windows\\sysnative;c:\\Windows\\sysnative\\Wbem"
+
+
+	$drive_letter	  = "p:"
+	$quartermaster_ip = "10.21.7.1"
+	$pxecfg		  = "pxe-cfg"
+	$unc		  = "\\\\${quartermaster_ip}\\${pxecfg}"
+	$hwtype		  ="01"
+	$vmfiles = [ "c:/vm/",
+   		     "c:/vm/bin",
+		     "c:/vm/vhd",
+	   	   ]
+
+
+        $ram                     = 1073741824
+#        $ram                    = 1024MB
+        $vhdsize                 = 21474836480
+#        $vhdsize                = 20GB
+	 $macaddress		 = "00-15-5d-d4-97-20"
+	 $mac = regsubst($macaddress, '(\-)','','G')
+
+	
+#        $mac                    = "00155DD49712"
+#        $vhdfile                = "test.vhdx"
+#        $vhdpath                = "c:/vm/vhd/test.vhdx"
+
+
+	$path = "${powershell_path};${path_extend};$::path"
+
+  file {$vmfiles:
+	ensure => directory,
+  }
+
+  file { "create_vm.ps1":
+	ensure => file,	
+	path   => "c:/vm/bin/create_vm.ps1",
+	source => "puppet:///modules/createvm/create_vm.ps1",
+	require => File["c:/vm/bin"],
+	
+  }
+
+
+   exec { "mount":
+	command => "c:/windows/system32/net.exe use ${drive_letter} \\\\${quartermaster_ip}\\${pxecfg} /persist:yes",
+	}
+
+   exec { "unmount":
+	command	    => "c:/windows/system32/net.exe use ${drive_letter} /delete",
+#	refreshonly => true,
+	require => File["macaddr_file"],
+	}
+
+   file { "macaddr_file":
+	path    => "p:/${hwtype}-${macaddress}",
+	ensure  => file,
+	content => template("createvm/mac.erb"),
+	require => Exec["mount"],
+#	notify  => Exec["unmount"],
+	}
+
+exec { "create_vm":
+        command     => "${powershell_path}\\powershell.exe -executionpolicy remotesigned -File c:/vm/bin/create_vm.ps1 -vmname ${mac} -ram ${ram} -vhdsize ${vhdsize} -mac ${mac} -vhdpath c:/vm/vhd/${mac}.vhdx",
+	cwd	    => "c:/vm/vhd",
+	creates     => "c:/vm/vhd/${mac}vhd",
+	require     =>  File["create_vm.ps1","c:/vm/vhd"],
+   }
+
+#exec { "new-vm":
+#	command     => "${powershell_path}\\powershell.exe -executionpolicy remotesigned -Command New-VM -Name ctrl-test -ComputerName ctrl-test -MemoryStartupBytes 1 GB -BootDevice LegacyNetworkAdapter -NewVHDPath c:\\vm\vhd\\testvm.vhdx -NewVHDSize 20 GB -Path c:\\vm\\vhd",
+#	cwd	    => "c:/vm/vhd",
+#	creates     => "c:/vm/vhd/testvm.vhdx",
+#        require     =>  File["create_vm.ps1","c:/vm/vhd"],
+#	refreshonly => true,
+#   }
+
+
+
+
+}
