1f327d19c097c2540cea7190852b57cd7c7dd9de
Peter J. Pouliot <ppouliot@gmail.com>
2016-03-31 21:42:38 +0100

    more alignement of parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,13 +7,36 @@
 # [*ensure_powershell*]
 #   Specify if the Hyper-V Module for Windows PowerShell will be installed
 #   in the host. Defaults to present. Valid values are: absent/present.
+#
 # [*ensure_tools*]
 #   Specify if the Hyper-V GUI Management Tools are installed. Defaults
 #   to absent. Valid values are: absent/present.
+#
 # [*virtual_hard_disks_folder*]
 #   Specify the default folder to store virtual hard disk files.
+#
 # [*virtual_machines_folder*]
 #   Specify the default folder to store virtual machines configuration files.
+#
+# [*enable*]
+#   Specify if the migration should be
+#   enabled or not in the hypervisor
+#
+# [*authentication_type*]
+#   Determine the authentication method
+#   used for migration: 'Kerberos' or 'CredSSP'
+#
+# [*allowed_networks*]
+#   Contains a comma separated list of the
+#   networks allowed to perform the migration.
+#   If this value is undef, any network will
+#   be allowed to perform the live migration.
+#
+# [*simultaneous_storage_migrations*]
+#   Number of simultaneous storage migrations
+#
+# [*simultaneous_live_migrations*]
+#   Number of simultaneous live migrations
 #
 # == Examples
 #
@@ -35,10 +58,16 @@
 #
 class hyper_v (
 
-  $ensure_powershell         = $hyper_v::params::ensure_powershell,
-  $ensure_tools              = $hyper_v::params::ensure_tools,
-  $virtual_hard_disks_folder = $hyper_v::params::virtual_hard_disks_folder
-  $virtual_machines_folder   = $hyper_v::params::virtual_machines_folder
+  $ensure_powershell               = $hyper_v::params::ensure_powershell,
+  $ensure_tools                    = $hyper_v::params::ensure_tools,
+  $virtual_hard_disks_folder       = $hyper_v::params::virtual_hard_disks_folder,
+  $virtual_machines_folder         = $hyper_v::params::virtual_machines_folder
+  $enable                          = $hyper_v::params::enable
+  $authentication_type             = $hyper_v::params::authentication_type,
+  $allowed_networks                = $hyper_v::params::allowed_networks,
+  $simultaneous_storage_migrations = $hyper_v::params::simultaneous_storage_migrations,
+  $simultaneous_live_migrations    = $hyper_v::params::simultaneous_live_migrations,
+
 
 ){
 
