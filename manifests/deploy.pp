8989be2dcff74f7e20d99b4d92a77878957f00aa
primeministerp <primeministerpete@gmail.com>
2016-04-05 22:38:41 -0400

    removing folders from deploy

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -21,23 +21,4 @@ class hyper_v::deploy () inherits hyper_v::params {
       require => Windowsfeature['Hyper-V'],
     }
   }
-
-
-  #
-  # Configure the host folders
-  #
-  exec{ 'ensure-virtual-hard-disk-folder':
-    command  => "Set-VMHost -VirtualHardDiskPath \"${virtual_hard_disks_folder}\"",
-    unless   => "if ((Get-VMHost).VirtualHardDiskPath -ne \"${virtual_hard_disks_folder}\") { exit 1 }",
-    provider => powershell,
-    require  => Windowsfeature['Hyper-V'],
-  }
-
-  exec{ 'ensure-virtual-machines-folder':
-    command  => "Set-VMHost -VirtualMachinePath \"${virtual_machines_folder}\"",
-    unless   => "if ((Get-VMHost).VirtualMachinePath -ne \"${virtual_machines_folder}\") { exit 1 }",
-    provider => powershell,
-    require  => Windowsfeature['Hyper-V'],
-  }
-
 }
